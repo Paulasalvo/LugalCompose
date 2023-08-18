3495fe829e7c9740a64f0b87c4dc762c63b049af
amonserrat <alvaro.monserrat@gmail.com>
2023-08-18 18:11:34 -0400

    Fix jitpack error

--- lugal/src/main/java/com/cuanto/lugal/ui/DialogMessage.kt
+++ lugal/src/main/java/com/cuanto/lugal/ui/DialogMessage.kt
@@ -2,19 +2,14 @@ package com.cuanto.lugal.ui
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.cuanto.lugal.common.body
-import com.cuanto.lugal.common.textButton
-import com.cuanto.lugal.common.title
 
 @Composable
 fun DialogMessage(
@@ -51,6 +46,7 @@ private val titleTextStyle = TextStyle(fontSize = 20.sp, fontWeight = FontWeight
 private val bodyTextStyle = TextStyle(fontSize = 14.sp)
 private val buttonTextStyle = TextStyle(fontSize = 16.sp)
 
+/*
 @Preview
 @Composable
 fun DialogMessagePreview() {
@@ -63,3 +59,4 @@ fun DialogMessagePreview() {
         )
     }
 }
+*/
